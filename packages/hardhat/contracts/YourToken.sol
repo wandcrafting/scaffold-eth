5cc7fac2746b5a83f3b01fce1476600231afa3e5
DhrumilShah <dhrumil.ketul@gmail.com>
2021-11-08 14:04:51 -0700

    update contract

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -1,10 +1,9 @@
 pragma solidity >=0.6.0 <0.7.0;
 
-//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-// learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract YourToken {
-    //ToDo: add constructor and mint tokens for deployer,
-    //you can use the above import for ERC20.sol. Read the docs ^^^
+contract YourToken is ERC20 {
+    constructor() ERC20("Token", "TKN") public {
+        _mint(msg.sender, 1000*10**18);
+    }
 }
-//
--- packages/hardhat/deploy/00_deploy_your_token.js
+++ packages/hardhat/deploy/00_deploy_your_token.js
@@ -14,7 +14,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
   //Todo: transfer tokens to frontend address
   const yourToken = await ethers.getContract("YourToken", deployer)
-  const result = await yourToken.transfer("YOUR_FRONT_END_ADDRESS", ethers.utils.parseEther("1000") );
+  const result = await yourToken.transfer("0x5cdD68A64C1bfF7eCd276cfc0532db7B15be9a86", ethers.utils.parseEther("1000") );
   /*
     // Getting a previously deployed contract
     const YourContract = await ethers.getContract("YourContract", deployer);
