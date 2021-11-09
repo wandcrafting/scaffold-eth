039e7eaf16f701213b72df712727a050bfef9b99
DhrumilShah <dhrumil.ketul@gmail.com>
2021-11-08 18:09:00 -0700

    checkpoint 3

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,20 +1,38 @@
 pragma solidity >=0.6.0 <0.7.0;
 
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
+contract Vendor is Ownable{
 
   YourToken yourToken;
+  uint256 public constant tokensPerEth = 100;
 
   constructor(address tokenAddress) public {
     yourToken = YourToken(tokenAddress);
   }
+  
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  
+  function buyTokens() public payable {
+    require(msg.value > 0, "no ether sent");
+    
+    uint256 amountOfTokens = msg.value * tokensPerEth;
+    
+    uint256 vendorBalance = yourToken.balanceOf(address(this));
+    require(vendorBalance >= amountOfTokens, "Vendor contract has not enough tokens in its balance");
 
-  //ToDo: create a payable buyTokens() function:
+    
+    (bool sent) = yourToken.transfer(msg.sender, amountOfTokens);
+    require(sent, "Failed to transfer token to user");
+    
+    emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+  }
+ 
+}
+
 
   //ToDo: create a sellTokens() function:
 
   //ToDo: create a withdraw() function that lets the owner, you can 
   //use the Ownable.sol import above:
-}
